3f71d11035405e89efab6e7ca13893289f2feed4
myparkdl-maker <myparkdl@gmail.com>
2026-09-04 08:42:32 +0900

    Update app.py

--- app.py
+++ app.py
@@ -45,7 +45,7 @@ def analyze_image_to_structured_data(image):
     ====
     """
     response = client.models.generate_content(
-        model='gemini-3.6-flash',
+        model='gemini-1.5-flash',
         contents=[image, prompt],
     )
     return response.text
